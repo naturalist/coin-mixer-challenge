fe79182558ace2319707197eb5c54096dc47300d
Stefan Geneshky <sge@pobox.com>
2018-01-25 21:04:52 -0800

    Make an iterator, add better tests

--- tests/mixer.js
+++ tests/mixer.js
@@ -1,54 +1,152 @@
 
 const sinon = require('sinon')
-const chai = require('chai')
-const expect = chai.expect
+const expect = require('chai').expect
 const mixer = require('../lib/mixer.js')
 const jobcoin = require('../lib/jobcoin.js')
 const config = require('config')
 
 describe('Mixer', ()=> {
   
-  var server
+  var refund, pay
 
-  describe('add', ()=> {
-    it('turns toAddress into array', ()=> {
-      mixer.add({fromAddress: 'aa', toAddress: 'bb'})
-      expect(mixer.wallet[0]).to.have.property('toAddress').with.lengthOf(1)
-    })
+  before(()=> {
+    refund = sinon.stub(jobcoin, "refund").callsFake((rec, onSuccess, onError) => { onSuccess() })
+    pay = sinon.stub(jobcoin, "pay").callsFake((address, amount, onSuccess, onCancel)=> { onSuccess() })
   })
 
-  describe('_process', ()=> {
-    
-    var stub
+  after(()=> {
+    refund.restore()
+    pay.restore()
+  })
 
-    before(()=> stub = sinon.stub(jobcoin, "send"))
-    after( ()=> stub.restore())
+  describe('iterator edge cases', ()=> {
+    var iterator
 
-    beforeEach(()=> {
+    before(()=> {
       mixer.wallet = [ 
         { fromAddress: 'aa', toAddress: [], amount: 0 },
-        { fromAddress: 'aa', toAddress: [], amount: 4 },
-        { fromAddress: 'aa', toAddress: ['bb', 'cc'], amount: 4 },
-        { fromAddress: 'xx', toAddress: ['yy', 'zz'], amount: 10 },
+        { fromAddress: 'aa', toAddress: [], amount: 4 }
       ]
+      iterator = mixer.makeIterator()
+    })
+
+    it('cleans records with no recepients and no amount', (done)=> {
+      iterator(() => {
+        expect(mixer.wallet).to.have.lengthOf(1)
+        expect(pay.calledOnce).to.be.false
+        expect(refund.calledOnce).to.be.false
+        done()
+      })
+    })
+
+    it('refunds and cleans records with no recepients, with amount', (done)=> {
+      iterator(() => {
+        expect(mixer.wallet).to.have.lengthOf(0)
+        expect(pay.calledOnce).to.be.false
+        expect(refund.calledOnce).to.be.true
+        done()
+      })
+    })
+  
+  })
+
+  describe('iterator mixed cases', ()=> {
+    var iterator
+
+    before(()=> {
+      mixer.wallet = [ 
+        { fromAddress: 'A0', toAddress: ['A1', 'A2'], amount: 2 },
+        { fromAddress: 'B0', toAddress: [], amount: 3 },
+      ]
+      iterator = mixer.makeIterator()
+    })
+
+    it('first run', (done)=> {
+      iterator(() => {
+        expect(mixer.wallet).to.have.lengthOf(2)
+        expect(mixer.wallet[0].toAddress).to.have.lengthOf(1)
+        expect(mixer.wallet[0].toAddress[0]).to.equal('A2')
+        expect(mixer.wallet[0].amount).to.equal(1)
+        done()
+      })
+    })
+
+    it('second run', (done)=> {
+      iterator(() => {
+        expect(mixer.wallet).to.have.lengthOf(1)
+        expect(mixer.wallet[0].toAddress).to.have.lengthOf(1)
+        expect(mixer.wallet[0].toAddress[0]).to.equal('A2')
+        expect(mixer.wallet[0].amount).to.equal(1)
+        done()
+      })
     })
     
-    it('removes records with no toAddress and no amount', ()=> {
-      mixer._process(0)
-      expect(mixer.wallet).to.have.lengthOf(3)
+  })
+
+  describe('iterator normal run', ()=> {
+
+    var iterator
+
+    before(()=> {
+      mixer.wallet = [ 
+        { fromAddress: 'A0', toAddress: ['A1', 'A2'], amount: 2 },
+        { fromAddress: 'B0', toAddress: ['B1', 'B2', 'B3'], amount: 3 },
+      ]
+      iterator = mixer.makeIterator()
     })
 
-    it('sends dangling balance back to sender', ()=> {
-      mixer._process(1)
-      expect(mixer.wallet).to.have.lengthOf(4)
-      expect(stub.calledWith(config.get('mixer.address'), 'aa', 4)).to.be.true
+    it('first run', (done)=> {
+      iterator(() => {
+        expect(mixer.wallet).to.have.lengthOf(2)
+        expect(mixer.wallet[0].toAddress).to.have.lengthOf(1)
+        expect(mixer.wallet[0].toAddress[0]).to.equal('A2')
+        expect(mixer.wallet[0].amount).to.equal(1)
+        done()
+      })
     })
 
-    it('processes unblocked records', ()=> {
-      expect(mixer.wallet[2]).to.have.property('toAddress').with.length(2)
-      mixer._process(2)
-      expect(stub.calledWith(config.get('mixer.address'), 'bb', 2)).to.be.true
-      expect(mixer.wallet[2]).to.have.property('toAddress').with.length(1)
+    it('second run', (done)=> {
+      iterator(()=> {
+        expect(mixer.wallet).to.have.lengthOf(2)
+        expect(mixer.wallet[1].toAddress).to.have.lengthOf(2)
+        expect(mixer.wallet[1].toAddress[0]).to.equal('B2')
+        expect(mixer.wallet[1].amount).to.equal(2)
+        done()
+      })
+    })
+
+    it('third run', (done)=> {
+      iterator(()=> {
+        expect(mixer.wallet).to.have.lengthOf(1)
+        expect(mixer.wallet[0].toAddress).to.have.lengthOf(2)
+        expect(mixer.wallet[0].toAddress[0]).to.equal('B2')
+        expect(mixer.wallet[0].amount).to.equal(2)
+        done()
+      })
+    })
+
+    it('fourth run', (done)=> {
+      iterator(()=> {
+        expect(mixer.wallet).to.have.lengthOf(1)
+        expect(mixer.wallet[0].toAddress).to.have.lengthOf(1)
+        expect(mixer.wallet[0].toAddress[0]).to.equal('B3')
+        expect(mixer.wallet[0].amount).to.equal(1)
+        done()
+      })
+    })
+
+    it('fifth run', (done)=> {
+      iterator(()=> {
+        expect(mixer.wallet).to.have.lengthOf(0)
+        done()
+      })
+    })
+
+    it('sixth run', (done)=> {
+      iterator(()=> {
+        expect(mixer.wallet).to.have.lengthOf(0)
+        done()
+      })
     })
 
   })
