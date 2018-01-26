2df38be05fda9945974fc965ae52b94926a83021
Stefan Geneshky <sge@pobox.com>
2018-01-26 08:42:14 -0800

    Change port for Heroku

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ var cb = function() {
 
 iterator(cb)
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(5000, () => console.log('Example app listening on port 3000!'))
